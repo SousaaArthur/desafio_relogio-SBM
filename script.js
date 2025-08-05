b3e950a21aab7d5d00ad8a0e6d13e0c7e0d1bcd9
Arthur Araújo Sousa <127199264+SousaaArthur@users.noreply.github.com>
2025-08-05 11:09:26 -0300

    feat: Armazenado tema no localStorage e feito a responsividade da pagina

--- script.js
+++ script.js
@@ -13,19 +13,45 @@ const root = document.documentElement;
 const textSeconds = document.getElementById("seconds");
 const textMinutes = document.getElementById("minutes");
 const textHours = document.getElementById("hour");
+const theme = localStorage.getItem('theme');
 
 // Variáveis de tempo inicial
 let hours = 23, minutes = 58, seconds = 55,  milliseconds = 1000;
-updateTime();
 
-// Armazena o id do setInterval
-let idSetInterval;
+// Chamada de funções
+updateTime();
+switchTheme(theme);
+console.log(`Página iniciada com o tema: ${theme}`)
+// applyThemeByHour(hours);
 
 // Inicia o botão play como false
 let playActive = false;
 
+// Armazena o id do setInterval
+let idSetInterval;
+
 buttonMoon.addEventListener('click', () => {
-  if(!buttonMoon.classList.contains("hidden")){
+  switchTheme('light');
+});
+
+buttonSun.addEventListener('click', () => {
+  switchTheme('night');
+});
+
+btnPlay.addEventListener('click', () => {
+  verifyButton(playActive, 'play');
+})
+
+btnPause.addEventListener('click', () => {
+  verifyButton(playActive, 'pause');
+});
+
+btnRestart.addEventListener('click', () => {
+  resetClock();
+});
+
+function switchTheme(theme = null){
+    if(theme === 'light'){
     buttonMoon.classList.add("hidden");
     buttonSun.classList.remove("hidden");
 
@@ -34,11 +60,10 @@ buttonMoon.addEventListener('click', () => {
     root.style.setProperty("--main-border", "#b5b5b5");
 
     root.style.setProperty("--bg-image", "url('../assets/day.jpg')");
-  }
-});
 
-buttonSun.addEventListener('click', () => {
-  if(!buttonSun.classList.contains("hidden")){
+    localStorage.setItem('theme', 'light');
+    console.log(`Tema alterado para: ${theme}`);
+  } else {
     buttonSun.classList.add("hidden");
     buttonMoon.classList.remove("hidden");
 
@@ -47,27 +72,29 @@ buttonSun.addEventListener('click', () => {
     root.style.setProperty("--main-border", "#343434");
 
     root.style.setProperty("--bg-image", "url('../assets/night.png')");
-  }
-});
 
-btnPlay.addEventListener('click', () => {
-  if(!playActive){
+    localStorage.setItem('theme', 'night');
+    console.log(`Tema alterado para: ${theme}`);
+  }
+}
+
+function verifyButton(value, type){
+  if(!value && type === 'play'){
     startClock(true);
-    console.log(`O relógio está: ${playActive}`);
-  }
-})
 
-btnPause.addEventListener('click', () => {
-  if(playActive){
+    btnPlay.classList.add("inative");
+    btnPause.classList.remove("inative");
+
+    console.log(`O relógio está: ${playActive ? 'Ativo' : 'Desativado'}`);
+  } else if(value && type === 'pause'){
     startClock(false);
-    console.log(`O relógio está: ${playActive}`);
-  }
-});
 
-btnRestart.addEventListener('click', () => {
-  resetClock();
-  console.log(`O relógio foi resetado!`);
-});
+    btnPause.classList.add("inative");
+    btnPlay.classList.remove("inative");
+
+    console.log(`O relógio está: ${playActive ? 'Ativo' : 'Desativado'}`);
+  }
+}
 
 function count(){
   seconds++;
@@ -76,18 +103,38 @@ function count(){
     seconds = 0;
     minutes++;
   }
-  
+
   if(minutes >= 60){
     minutes = 0;
     hours++;
-  } 
-  
+  }
+
+  // applyThemeByHour(hours);
+
   if(hours >= 24){
     resetClock();
   } else {
     updateTime();
   }
 }
+
+// function applyThemeByHour(hours){
+//     if(hours >= 6 && hours < 14){
+//     root.style.setProperty("--bg-blur", "rgba(169, 169, 169, 0.35)");
+//     root.style.setProperty("--color-text", "#454545");
+//     root.style.setProperty("--main-border", "#b5b5b5");
+
+//     root.style.setProperty("--bg-image", "url('../assets/day.jpg')");
+//   } else if (hours >= 14 && hours < 19){
+//     root.style.setProperty("--bg-image", "url('../assets/afternoon.png')");
+//   } else if (hours >= 19){
+//     root.style.setProperty("--bg-blur", "rgba(21, 21, 21, 0.35)");
+//     root.style.setProperty("--color-text", "#fafafa");
+//     root.style.setProperty("--main-border", "#343434");
+
+//     root.style.setProperty("--bg-image", "url('../assets/night.png')");
+//   }
+// }
 
 function startClock(active){
   if(active){
@@ -102,6 +149,7 @@ function startClock(active){
 
 function resetClock(){
   hours = 0, minutes = 0, seconds = 0;
+  console.log(`O tempo foi resetado!`);
   updateTime();
 }
 
@@ -109,4 +157,5 @@ function updateTime(){
   textSeconds.innerText = seconds.toString().padStart(2, '0');
   textMinutes.innerText = minutes.toString().padStart(2, '0');
   textHours.innerText = hours.toString().padStart(2, '0');
+  console.log(`Tempo atualizado!`);
 }
